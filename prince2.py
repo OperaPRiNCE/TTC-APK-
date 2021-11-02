8556f9658568926d23e4a4af15ae855c90f84ca7
OperaPRiNCE <mdshaun114750@gmail.com>
2021-11-02 00:49:24 -0700

    project 2
    It's time to rock

---
+++ prince2.py
@@ -0,0 +1,10 @@
+def sj():
+    for num in range (1,100):
+
+        if num>1:
+            for i in range(2,num):
+                if(num % i)==0:
+                    break
+            else:
+                print("result+",num)
+sj()
